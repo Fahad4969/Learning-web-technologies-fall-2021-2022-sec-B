094e9beae1d9727ab9f89a29c6b88a31d6ca9d57
Fahad Mollah <fahadmollah43@gmail.com>
2021-10-11 02:54:25 +0600

    LAB-5 Session Tracking
    l5

---
+++ LAB-5 Session Tracking/profilePicture.php
@@ -0,0 +1,44 @@
+<?php
+	session_start();
+	if(isset($_SESSION['flag'])){
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Change Profile picture</title>
+</head>
+<body>
+   <div>
+	<form action="" method="post">
+		<fieldset>
+			<legend>Change Profile picture</legend>
+			<table>
+               <tr>
+                   <td>Upload Profile picture</td>
+               </tr>
+               <tr>
+                   <td><input type="file" name="fileToUpload" id="fileToUpload"></td>
+               </tr>
+				<tr>
+					<td><input type="submit" name="submit" value="Upload"></td>
+				</tr>
+			</table>
+		</fieldset>
+	</form>
+</div>
+    <div class="dashboard">
+        <a href="./Home.html">Home</a> <br>
+        <a href="./viewProfile.php">View Profile</a> <br>
+        <a href="./EditProfile.php">Edit Profile</a><br>
+        <a href="./profilePicture.php">Change Profile picture</a><br>
+        <a href="./ChangePassword.php">Change Password</a><br>
+        <a href="./logout.php">logout</a> <br>
+    </div>
+</body>
+</html>
+<?php
+	
+	}else{
+		header('location: login.html');
+	}
+?>
