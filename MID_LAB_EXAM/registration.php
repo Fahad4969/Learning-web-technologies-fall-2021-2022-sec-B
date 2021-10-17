d04127212a740b3a03de64c09a086ed2032b0c48
Fahad Mollah <fahadmollah43@gmail.com>
2021-10-17 12:18:48 +0600

    v1

---
+++ MID_LAB_EXAM/registration.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form>
+		<fieldset>
+			<legend>REGISTRATION</legend>
+			<table>
+				<tr>
+					<td>
+						<label>Id</label>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="number" name="number">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label>Password</label>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="Password" name="Password">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label>Confirm password</label>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="Password" name="Password">
+					</td>
+				</tr>
+				<tr>
+				<td>
+					<label>Name</label>
+				</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="txt" name="name">
+					</td>
+				</tr>
+				<tr>
+					<td>
+                    <label>User Type</label><br>
+                    <label>___________</label><br>
+					<form>
+  <input type="radio"  name="user" value="user">
+  <label for="male">User</label>
+  <input type="radio"  name="Admin" value="Admin">
+  <label for="female">Admin</label><br>
+</form> 
+</td>
+</tr>
+<tr>
+	<td>
+		<input type="submit" name="submit">
+		<a href="signin.html">Sign in</a>
+	</td>
+</tr>
+			</table>
+		</fieldset>
+	</form>
+</body>
+</html>
